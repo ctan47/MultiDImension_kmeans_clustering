00cec91c381d7372f534f45a89fe79f4826196f7
Chetan Hatkar <44762312+ctan47@users.noreply.github.com>
2023-04-26 14:09:38 +0530

    Update random.cpp

--- random.cpp
+++ random.cpp
@@ -2,11 +2,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-	int a;
+	int a,r;
 	cout<<"Enter a Number\n";
 	cin>>a;
-	for(int i=0;i<a;i++)
-	cout<<" "<<(rand()*2+rand())%99;
-	
+	for(int i=0;i<a;i++){
+	r=(rand()*2+rand())%99;
+	if(r<0)
+        r=r*-1;
+	cout<<r<<" ";	
+	}
  return 0;	
 }
